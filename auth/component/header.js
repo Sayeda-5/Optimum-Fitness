47abd893eadaf8208ce676f9e88326d2f297ccc4
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-12-14 22:13:27 +0530

    Log in or sign up done

---
+++ auth/component/header.js
@@ -0,0 +1,13 @@
+function header() {
+  return `
+    <div id="logo">
+        <a href="">
+            <img src="https://cdn1.cronometer.com/brand2/Cronometer_MainLogo_FloralWhite_Mandarin.svg"
+                alt="Cronometer Logo">
+        </a>
+    </div>
+    `;
+}
+
+
+export {header};
---
+++ auth/script/login.js
@@ -0,0 +1,5 @@
+import { header } from "../component/header.js";
+
+let head=document.getElementById('header');
+
+head.innerHTML=header();
