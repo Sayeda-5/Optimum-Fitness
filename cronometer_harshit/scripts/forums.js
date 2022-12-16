a0fa2f0aa6bc44f786010f5f5b2de90b0232192d
hrsht339 <harsit.sahu@gmail.com>
2022-12-17 01:10:31 +0530

    loader

--- cronometer_harshit/scripts/forums.js
+++ cronometer_harshit/scripts/forums.js
@@ -1,1 +1,26 @@
-let
+
+signin = document.querySelector("#signin")
+register = document.querySelector("#resgister")
+loader = document.querySelector("#loader")
+main = document.querySelector("#main")
+about = document.querySelector(".navbar-link")
+
+
+
+
+signin.addEventListener("click",()=>{
+    main.style.display="none"
+    loader.style.display="flex"
+    setTimeout(()=>{
+        location.href="auth\login.html"
+    },1000)
+})
+
+
+about.addEventListener("click",()=>{
+    main.style.display="none"
+    loader.style.display="flex"
+    setTimeout(()=>{
+        location.href="./about.html"
+    },1000)
+})
