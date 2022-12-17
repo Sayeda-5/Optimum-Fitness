e7086411233f75d9cc7e3b0d4ef3722a1cf08c8a
hrsht339 <harsit.sahu@gmail.com>
2022-12-17 22:31:24 +0530

    loader

--- cronometer_harshit/scripts/forums.js
+++ cronometer_harshit/scripts/forums.js
@@ -1,26 +1,35 @@
 
-signin = document.querySelector("#signin")
-register = document.querySelector("#resgister")
+// signin = document.querySelector("#signin")
+// register = document.querySelector("#resgister")
 loader = document.querySelector("#loader")
 main = document.querySelector("#main")
-about = document.querySelector(".navbar-link")
+// about = document.querySelector(".navbar-link")
 
 
 
 
-signin.addEventListener("click",()=>{
+// signin.addEventListener("click",()=>{
+//     main.style.display="none"
+//     loader.style.display="flex"
+//     setTimeout(()=>{
+//         location.href="auth\login.html"
+//     },1000)
+// })
+
+window.onload = () => {
     main.style.display="none"
     loader.style.display="flex"
     setTimeout(()=>{
-        location.href="auth\login.html"
+        main.style.display="block"
+        loader.style.display="none"
     },1000)
-})
+  };
 
 
-about.addEventListener("click",()=>{
-    main.style.display="none"
-    loader.style.display="flex"
-    setTimeout(()=>{
-        location.href="./about.html"
-    },1000)
-})
+// about.addEventListener("click",()=>{
+//     main.style.display="none"
+//     loader.style.display="flex"
+//     setTimeout(()=>{
+//         location.href="./about.html"
+//     },1000)
+// })
